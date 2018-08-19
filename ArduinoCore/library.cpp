349ef4b33994f2d6a40c574eaf12cabdbbeda76d
LAPTOP-MNMV8UG3\Vulcan <1079021229@qq.com>
2018-08-20 01:35:32 +0800

    Initial commit

---
+++ ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 2018/8/20 1:33:45
+ * Author : Vulcan
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ sketch_feb03a/Sketch.cpp
@@ -0,0 +1,31 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(A0,OUTPUT);
+  pinMode(A1,OUTPUT);
+  digitalWrite(A0,LOW);
+  digitalWrite(A1,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.write(0x55);
+  delay(10);
+  Serial.write(0xAA);
+  delay(10);
+  Serial.write(0x02);
+  delay(10);
+  Serial.write(map(analogRead(A2),0,1024,0,255));
+  delay(10);
+  Serial.write(map(analogRead(A3),0,1024,0,255));
+  delay(100);
+}
